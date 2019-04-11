b9d7a4167f970c255fe43c18e812810872f3a477
Lena Orobei <oorobei@magento.com>
2019-04-11 14:01:53 -0500

    Hackathon: added dependencies, fixed cron

--- app/code/Unicorn/MagicUpdate/Cron/UpdateModuleCron.php
+++ app/code/Unicorn/MagicUpdate/Cron/UpdateModuleCron.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Unicorn\MagicUpdate\Cron;
 
@@ -10,15 +11,27 @@ use Unicorn\MagicUpdate\Model\ModuleList;
 
 class UpdateModuleCron
 {
+    /**
+     * @var ModuleList
+     */
+    private $moduleList;
+
+    /**
+     * UpdateModuleCron constructor.
+     * @param ModuleList $moduleList
+     */
     public function __construct(
         ModuleList $moduleList
     ) {
         $this->moduleList = $moduleList;
     }
 
+    /**
+     * @return $this
+     */
     public function execute()
     {
         $this->moduleList->doSafeUpdate();
+        return $this;
     }
-
 }
